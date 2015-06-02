4e8abf01dfbdba1e738ea9e3fb7de154d68cb891
Viljami Peltola <viljami.peltola@iki.fi>
2015-06-02 21:07:55 +1000

    Initialised module allowed to undefined fix.

--- module.js
+++ module.js
@@ -5,48 +5,44 @@
   var isWindowLoaded = false;
 
   var loader = function (name, dependencies, fn){
-    queue[name].result = fn.apply(
-      undefined,
-      dependencies && dependencies.map(function (s){
+    queue[name].result = fn.apply(undefined, dependencies && 
+      dependencies.map(function (s){
         if (! queue[s]) throw new Error('module not defined: ' + s);
         if (queue[s].loaded) return queue[s].result;
         return loader(s, queue[s].deps, queue[s].fn);
       }));
-
+    // Allow modules to return undefined.
     queue[name].loaded = true;
     return queue[name].result;
   };
 
-  // init modules
+  // Init modules.
   window.addEventListener('load', function (){
-    setTimeout(function (){ // defer
+    setTimeout(function (){ // defer ensures all module calls have happened
       isWindowLoaded = true;
-      Object.keys(queue)
-      .forEach(function(name){
-        if (queue[name].result) return;
-        loader(name, queue[name].deps, queue[name].fn);
+      Object.keys(queue).forEach(function(name){
+        if (! queue[name].loaded) loader(name, queue[name].deps, queue[name].fn);
       });
     }, 10);
   });
 
   return function (moduleName, dependencies, fn){
+    var result;
     if (arguments.length === 1) return queue[moduleName].result;
-    
-    var result, moduleLoaded = false;
     if (! fn){
       fn = dependencies;
       dependencies = undefined;
       result = fn();
-      moduleLoaded = true;
     }
 
     queue[moduleName] = {
       fn: fn,
       deps: dependencies,
       result: result,
-      loaded: moduleLoaded
+      loaded: !! result
     };
 
-    if (isWindowLoaded && ! moduleLoaded) return loader(moduleName, dependencies, fn);
+    // Load module and return it initialised if module called after page load.
+    if (isWindowLoaded && ! result) return loader(moduleName, dependencies, fn);
   };
 })(window);
