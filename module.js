28536d310ad8bce9f84ae8f3cbb6867595e891f1
Viljami Peltola <viljami.peltola@iki.fi>
2015-06-02 20:32:03 +1000

    Module loader v0.0.1

---
+++ module.js
@@ -0,0 +1,62 @@
+/* globals window */
+
+'use strict';
+
+;window.module = (function(window, undefined){
+  var queue = {};
+  var rootScope = {};
+  var loaded = false;
+
+  window.addEventListener('load', function(){
+    setTimeout(function(){
+      loaded = true;
+      Object.keys(queue)
+      .forEach(function(name){
+        if (queue[name].result) return;
+        loader(name, queue[name].deps, queue[name].fn);
+      });
+    }, 10);
+  });
+
+  return function(moduleName, dependencies, fn){
+    if (arguments.length === 1) return queue[moduleName].result;
+    var result, moduleLoaded = false;
+    if (! fn){
+      fn = dependencies;
+      dependencies = undefined;
+      result = fn();
+      moduleLoaded = true;
+    }
+
+    if (! loaded){
+      queue[moduleName] = {
+        deps: dependencies,
+        fn: fn,
+        result: result,
+        loaded: moduleLoaded
+      };
+      return;
+    }
+
+    queue[moduleName] = {
+      fn: fn,
+      deps: dependencies,
+      result: result,
+      loaded: moduleLoaded
+    };
+    if (! moduleLoaded) loader(moduleName, dependencies, fn)
+  };
+
+  function loader (name, dependencies, fn){
+    queue[name].result = fn.apply(
+      rootScope,
+      dependencies && dependencies.map(function(s){
+        if (! queue[s]) throw new Error('module not defined: ' + s);
+        if (queue[s].loaded) return queue[s].result;
+        return loader(s, queue[s].deps, queue[s].fn);
+      }));
+
+    queue[name].loaded = true;
+    return queue[name].result;
+  }
+})(window);
